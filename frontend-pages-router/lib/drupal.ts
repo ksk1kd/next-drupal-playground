87c8e31a553f8002a415054aadd798de7be95d49
Keisuke Ikeda <ksk1kd.dev@gmail.com>
2025-03-01 22:18:09 +0900

    install pages router frontend

---
+++ frontend-pages-router/lib/drupal.ts
@@ -0,0 +1,16 @@
+import { NextDrupalPages } from "next-drupal"
+
+const baseUrl = process.env.NEXT_PUBLIC_DRUPAL_BASE_URL as string
+const clientId = process.env.DRUPAL_CLIENT_ID as string
+const clientSecret = process.env.DRUPAL_CLIENT_SECRET as string
+
+export const drupal = new NextDrupalPages(baseUrl, {
+  // Enable to use authentication
+  // auth: {
+  //   clientId,
+  //   clientSecret,
+  // },
+  // withAuth: true,
+  useDefaultEndpoints: true,
+  // debug: true,
+})
